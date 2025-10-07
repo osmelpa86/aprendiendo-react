24feebbf345e66398b3fe0da64c8651aa844d8ff
osmelpa86 <osmelpa86@gmail.com>
2025-10-07 09:08:46 -0600

    twitter-following-card

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard";
+
+function App() {
+  return (
+    <div className="App">
+      <TwitterFollowCard userName="osmelpa86" name="Osmel Perez Alzola" />
+      <TwitterFollowCard userName="osmelpa86" name="Osmel Perez Alzola" />
+      <TwitterFollowCard userName="osmelpa86" name="Osmel Perez Alzola" />
+      <TwitterFollowCard userName="osmelpa86" name="Osmel Perez Alzola" />
+    </div>
+  );
+}
+
+export default App;
